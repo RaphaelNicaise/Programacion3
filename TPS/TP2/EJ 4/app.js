cf3365f73ebed2e44c17b35fccb7dea5f45a37df
NACXIIX <nicocordano@hotmail.com>
2025-04-17 01:26:22 -0300

    ejercicio 4 del TP 2 en progreso

---
+++ TPS/TP2/EJ 4/app.js
@@ -0,0 +1,6 @@
+let input = document.getElementById("input-text");
+let btnEnviar = document.getElementById("boton-enviar")
+
+btnEnviar.addEventListener("click", () => {
+    console.log(input.value)
+})
