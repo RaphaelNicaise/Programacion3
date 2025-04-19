10a2775dab2308c009895005b66310d18728e9f3
NACXIIX <nicocordano@hotmail.com>
2025-04-18 21:03:07 -0300

    ejercicio 4 del TP 2 terminado

--- TPS/TP2/EJ 4/app.js
+++ TPS/TP2/EJ 4/app.js
@@ -1,6 +1,24 @@
-let input = document.getElementById("input-text");
-let btnEnviar = document.getElementById("boton-enviar")
+const input = document.getElementById("input-text");
+const btnEnviar = document.getElementById("boton-enviar");
+const formulario = document.getElementById("form");
+const ul = document.getElementById("lista")
+
+formulario.addEventListener("submit", (event) => {
+    event.preventDefault();
+})
 
 btnEnviar.addEventListener("click", () => {
-    console.log(input.value)
+    const contenidoInput = input.value
+    const li = document.createElement("li")
+    li.innerText = contenidoInput
+    ul.appendChild(li)
+    
+    li.addEventListener("click", () => {
+        li.classList.add("completado")
+    })
+
 })
+
+
+
+
