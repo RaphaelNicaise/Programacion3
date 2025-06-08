72e62e26a3ad0b0f88f1f7adf7aac1392ea13f6b
NACXIIX <nicocordano@hotmail.com>
2025-06-08 20:39:56 -0300

    Algunos cambios en el front

--- TPS/TP3/src/routes/views.routes.js
+++ TPS/TP3/src/routes/views.routes.js
@@ -5,12 +5,17 @@ const TurnosViewController = require('../controllers/views/turnos.views.controll
 const {loginViewController} = require('../controllers/home/loginView.controller.js')
 const rutaVistas = Router()
 
+//rutas pacientes
+
 // OJO: en las vistas pasa esto
 // con verifyToken salta esto: {"message":"Token de acceso no proporcionado"}, sin el verify token se dirige al endpoint correctamente
 rutaVistas.get('/pacientes',/*verifyTokenViewsMiddleware,*/ PacientesViewController.getPacientesView);
 
 rutaVistas.post('/pacientes/eliminar', PacientesViewController.borrarPaciente) // ESTA RUTA ES UN DELETE, SI NO USAR FETCH
 rutaVistas.post('/', PacientesViewController.crearPaciente);
+
+
+//rutas turnos
 
 rutaVistas.get('/turnos',/*verifyTokenViewsMiddleware,*/ TurnosViewController.getTurnosView);
 rutaVistas.post('/', TurnosViewController.crearTurno);
